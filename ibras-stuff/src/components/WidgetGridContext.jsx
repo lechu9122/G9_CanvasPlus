42c0195738a913aaccaabfc8b67c1a807a54b7c4
iwah144 <161651144+iwah144@users.noreply.github.com>
2025-08-17 15:53:34 +1200

    made widget grid and widget two separate files

---
+++ ibras-stuff/src/components/WidgetGridContext.jsx
@@ -0,0 +1,6 @@
+// =============================
+// File: /components/WidgetGridContext.jsx
+// =============================
+import { createContext } from "react";
+
+export const GridCtx = createContext(null);
